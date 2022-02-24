5d42bb4a0d54e8ede50324044faea3e6ab97646b
dimitri20 <45402354+dimitri20@users.noreply.github.com>
2022-02-24 14:38:23 +0400

    Update crackTkt.py

--- crackTkt.py
+++ crackTkt.py
@@ -1,1 +1,30 @@
+import random
 
+string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+testStr = ""
+generatedStrings = []
+
+for i in range(23982224839372800):
+    
+    for i in range(10):
+        testStr += random.choice(string)
+    
+    if testStr not in generatedStrings:
+        generatedStrings.append(testStr)
+        print(testStr)
+        
+    testStr = ""
+
+
+def generateStrings():
+    string = ""
+    for i in range(97, 123):
+        string+=chr(i)
+    
+    for i in range(65, 91):
+        string+=chr(i)
+    
+    for i in range(10):
+        string += str(i)
+        
+    return string
